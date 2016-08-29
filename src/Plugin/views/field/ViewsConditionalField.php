733a382d94ea899e5cae7195d1a4e871666e2cb9
Rohit Joshi <rohit.joshi@acquia.com>
2016-08-29 14:05:29 +0530

    Adding handler for field and hook_views_data().

---
+++ src/Plugin/views/field/ViewsConditionalField.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\views_conditional\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\NodeType;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Views field handler for conditional field.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("views_conditional")
+ */
+class ViewsConditionalField extends FieldPluginBase {
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * Define the available options
+   * @return array
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['node_type'] = array('default' => 'article');
+
+    return $options;
+  }
+
+  /**
+   * Provide the options form.
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    $types = NodeType::loadMultiple();
+    $options = [];
+    foreach ($types as $key => $type) {
+      $options[$key] = $type->label();
+    }
+    $form['node_type'] = array(
+      '#title' => $this->t('Which node type should be flagged?'),
+      '#type' => 'select',
+      '#default_value' => $this->options['node_type'],
+      '#options' => $options,
+    );
+
+    parent::buildOptionsForm($form, $form_state);
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $node = $values->_entity;
+    if ($node->bundle() == $this->options['node_type']) {
+      return $this->t('Hey, I\'m of the type: @type', array('@type' => $this->options['node_type']));
+    }
+    else {
+      return $this->t('Hey, I\'m something else.');
+    }
+  }
+}
